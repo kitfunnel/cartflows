37a34c3ab41d1ccf4e60dde6f0f93b7514fe8ada
kitfunnel <111517579+kitfunnel@users.noreply.github.com>
2023-11-20 22:04:00 -0500

    Clear

--- admin-core/assets/build/settings-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd3004b74e4f333695f59');
--- wizard/assets/build/wizard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '991783b20719fef151a6');
