9f493b25fe95795791d8347105f5be2f0dab3c25
kitfunnel <111517579+kitfunnel@users.noreply.github.com>
2023-12-07 16:08:12 -0500

    Limpieza

--- admin-core/assets/build/editor-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ec9f73752ac5c38957da');
--- admin-core/assets/build/settings-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e1092efe3f8043ac1f89');
